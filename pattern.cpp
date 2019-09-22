ddb15f4f8029426321a224b12007d04659d56c34
Varinderr <55649134+Varinderr@users.noreply.github.com>
2019-09-22 12:51:14 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number of rows:";
+	cin>>n;
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			cout<<" * ";
+		}
+		cout<<"\n";
+	}
+	getch();
+}
