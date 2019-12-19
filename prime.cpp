758d76df33eef513be58db033da905bc87d4e5b8
Varinderr <55649134+Varinderr@users.noreply.github.com>
2019-12-19 15:21:41 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 #include<conio.h>
+using namespace std;
 int main()
 {
-	
+	int i,n;
+	bool isprime = true; 
+
+	cout<<"enter number"<<endl;
+	cin>>n;
+
+	for(i=2; i <= n/2; i++)
+{
+	if(n % i == 0)
+	{
+		isprime= false;
+		break;
+	}
+}
+if (isprime)
+{
+	cout<<"this is prime";
+}
+else
+{
+cout<<"this isn't prime";
+}
 	getch();
+
 }
