4ea9134cf43393e0813c62e37258d6fecf205170
Varinderr <55649134+Varinderr@users.noreply.github.com>
2019-12-19 17:20:59 +0530

    Add files via upload

---
+++ pattern diamond.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int i,j,n,k;//i=rows,n=no of rows, j=space,k=star
+	cout<<"enter number of rows:";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		for(j=n-i;j>=1;j--)
+		{
+			cout<<" ";
+		}
+		for(k=1;k<=2*i-1;k++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+
+	for(i=n-1;i>=1;i--)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			cout<<" ";
+		}
+		for(k=1;k<=2*i-1;k++)
+		{
+			cout<<"*";
+		}
+				cout<<endl;	
+
+	}
+	getch();
+}
